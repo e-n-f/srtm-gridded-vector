69ea4c4b69faf604b934bf83b0a4c97418b6d07c
Eric Fischer <enf@pobox.com>
2017-11-16 12:35:34 -0800

    Read data, write GeoJSON

--- dem2json.cpp
+++ dem2json.cpp
@@ -40,12 +40,13 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	int nrows = -1;
-	int ncols = -1;
+	size_t nrows = 0;
+	size_t ncols = 0;
 	double ulxmap = -999;
 	double ulymap = -999;
 	double xdim = 0;
 	double ydim = 0;
+	int nodata = 0;
 
 	FILE *hdr = fopen(argv[1], "r");
 	if (hdr == NULL) {
@@ -72,8 +73,62 @@ int main(int argc, char **argv) {
 		if (startswith(s, "YDIM")) {
 			ydim = atof(word2(s));
 		}
+		if (startswith(s, "NODATA")) {
+			nodata = atoi(word2(s));
+		}
 	}
 	fclose(hdr);
 
-	printf("%d %d %f %f %f %f\n", nrows, ncols, ulxmap, ulymap, xdim, ydim);
+	std::vector<std::vector<short>> vals;
+
+	FILE *data = fopen(argv[2], "rb");
+	if (data == NULL) {
+		perror(argv[2]);
+		exit(EXIT_FAILURE);
+	}
+
+	for (size_t y = 0; y < nrows; y++) {
+		vals.push_back(std::vector<short>());
+
+		for (size_t x = 0; x < ncols; x++) {
+			int c = getc(data);
+			int c1 = getc(data);
+
+			if (c == EOF || c1 == EOF) {
+				fprintf(stderr, "short read\n");
+				exit(EXIT_FAILURE);
+			}
+
+			int val = c * 256 + c1;
+
+			vals[y].push_back(val);
+		}
+	}
+
+	fclose(data);
+
+	for (size_t zoom = 0; zoom <= 5; zoom++) {
+		size_t increment = 3 * (1 << (9 - zoom));
+
+		for (size_t y = 0; y < nrows; y += increment) {
+			for (size_t x = 0; x < ncols; x += increment) {
+				double sum = 0;
+				size_t count = 0;
+
+				for (size_t xx = x; xx < x + increment; xx++) {
+					for (size_t yy = y; yy < y + increment; yy++) {
+						if (vals[y][x] != nodata) {
+							sum += vals[y][x];
+							count++;
+						}
+					}
+				}
+
+				if (count > 0) {
+					printf("{\"type\": \"Feature\", \"properties\":{\"elevation\":%f}, \"tippecanoe\":{\"minzoom\":%zu, \"maxzoom\": %zu}, \"geometry\": {\"type\": \"Point\", \"coordinates\" : [ %f, %f ] } }\n",
+					       sum / count, zoom, zoom, ulxmap + x * xdim, ulymap - y * ydim);
+				}
+			}
+		}
+	}
 }
